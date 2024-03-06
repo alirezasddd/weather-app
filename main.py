816229be3310463011f4b04169283c7e52ed51a0
Alireza <alireza.sad.ger@gmail.com>
2024-03-07 01:15:51 +0330

    labels updated

--- main.py
+++ main.py
@@ -47,34 +47,65 @@ frame_label.pack(pady=6, side=tk.BOTTOM)
 
 # City Name:
 city_label = tk.Label(root, font=("arial", 30, "bold"), fg="#e355cd")
+
 city_label.place(x=120, y=160)
 
 # Time:
 time_label = tk.Label(root, font=("arial", 15, "bold"), fg="#e355cd")
+
 time_label.place(x=120, y=230)
 
 clock_label = tk.Label(root, font=("arial", 15, "bold"), fg="#e355cd")
+
 clock_label.place(x=120, y=270)
 
 # Labels :
-label1 = tk.Label(root, text="WIND", font=("Helvetica", 13, "bold"),
-                  fg="white", bg="#1ab5ef")
+label_one = tk.Label(root, text="WIND", font=("Helvetica", 13, "bold"),
+                     fg="white", bg="#1ab5ef")
 
-label1.place(x=140, y=400)
+label_one.place(x=140, y=400)
 
-label2 = tk.Label(root, text="HUMINITY", font=("Helvetica", 13, "bold"),
-                  fg="white", bg="#1ab5ef")
+label_two = tk.Label(root, text="HUMINITY", font=("Helvetica", 13, "bold"),
+                     fg="white", bg="#1ab5ef")
 
-label2.place(x=280, y=400)
+label_two.place(x=280, y=400)
 
-label3 = tk.Label(root, text="DESCRIPTION", font=("Helvetica", 13, "bold"),
-                  fg="white", bg="#1ab5ef")
+label_three = tk.Label(root, text="DESCRIPTION", font=("Helvetica", 13, "bold"),
+                       fg="white", bg="#1ab5ef")
 
-label3.place(x=450, y=400)
+label_three.place(x=450, y=400)
 
-label4 = tk.Label(root, text="PRESSURE", font=("Helvetica", 13, "bold"),
-                  fg="white", bg="#1ab5ef")
+label_four = tk.Label(root, text="PRESSURE", font=("Helvetica", 13, "bold"),
+                      fg="white", bg="#1ab5ef")
 
-label4.place(x=650, y=400)
+label_four.place(x=650, y=400)
+
+temp_label = tk.Label(root, font=("arial", 70, "bold"), fg="#e355cd")
+
+temp_label.place(x=590, y=170)
+
+condition_label = tk.Label(root, font=("arial", 15, "bold"), fg="#4b4bcc")
+
+condition_label.place(x=590, y=270)
+
+wind_label = tk.Label(root, text="...", font=("arial", 20, "bold"),
+                      bg="#1ab5ef", fg="#404040")
+
+wind_label.place(x=140, y=430)
+
+humidity_label = tk.Label(root, text="...", font=("arial", 20, "bold"),
+                          bg="#1ab5ef", fg="#404040")
+
+humidity_label.place(x=280, y=430)
+
+description_label = tk.Label(root, text="...", font=("arial", 20, "bold"),
+                             bg="#1ab5ef", fg="#404040")
+
+description_label.place(x=450, y=430)
+
+pressure_label = tk.Label(root, text="...", font=("arial", 20, "bold"),
+                             bg="#1ab5ef", fg="#404040")
+
+pressure_label.place(x=650, y=430)
 
 root.mainloop()
